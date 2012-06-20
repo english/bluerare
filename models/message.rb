57a826aabf0d297b67723f489bea27f8240f3738
Jamie English <jamienglish@gmail.com>
2012-06-20 20:45:29 +0100

    Message model

--- models/message.rb
+++ models/message.rb
@@ -1,3 +1,14 @@
 class Message
+  include ActiveModel::Validations
 
+  validates_presence_of :name, :email, :content
+
+  validates_format_of :email, :message => "Invalid email address",
+    :with => /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/
+
+  attr_accessor :name, :email, :content
+
+  def initialize attrs={}
+    attrs.each { |k,v| send "#{k}=", v }
+  end
 end
---
+++ test/models/test_message.rb
@@ -0,0 +1,42 @@
+gem 'minitest'
+require 'minitest/autorun'
+
+require 'active_model'
+require_relative '../../models/message'
+
+class MessageTest < Minitest::Unit::TestCase
+  def subject
+    @message ||= Message.new(:name => "John Smith",
+                             :email => "john@example.com",
+                             :content => "Bla bla bla")
+  end
+
+  def test_name_required
+    subject.name = nil
+    refute subject.valid?
+
+    subject.name = "John"
+    assert subject.valid?
+  end
+
+  def test_email_validations
+    subject.email = nil
+    refute subject.valid?
+
+    subject.email = "someone@example.com"
+    assert subject.valid?
+
+    subject.email = "someone.example.com"
+    refute subject.valid?
+    assert subject.errors[:email].include?("Invalid email address"),
+      "Expected 'Invalid email address', got: #{subject.errors[:email].inspect}"
+  end
+
+  def test_content_required
+    subject.content = nil
+    refute subject.valid?
+
+    subject.content = "Some content"
+    assert subject.valid?
+  end
+end
